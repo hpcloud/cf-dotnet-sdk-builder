a4bdb36773d8fd6bf3748125b8f4f9b2a535a620
Vlad Iovanov <vlad.iovanov@hpe.com>
2016-03-15 18:22:42 -0700

    Don't use our own gem lockfile and use rake tasks for db creation

--- cf-doc-generation/update-code.sh
+++ cf-doc-generation/update-code.sh
@@ -2,32 +2,16 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# Generate Cloud Foundry API Doc templates
-# gem version should be 2.3.0
-# gem update --system 2.3.0
-
-export TASKS=spec:api
+export TASKS="tmp:clear db:drop db:create spec:api"
 export DB=mysql
 
-# needs mysql connector from http://dev.mysql.com/get/Downloads/Connector-C/mysql-connector-c-6.1.5-win32.zip
-# Make sure not to use a directory with spaces ' ' in it.
-# bundle config build.mysql2 --with-mysql-dir="E:\Tools\mysql-connector-c-6.1.5-win32"
-# TODO: vladi: make a subcommand for the setup part?
-#bundle install --deployment --without development
-
-export BUNDLE_GEMFILE=${DIR}/Gemfile-cf-docgen
-bundle install --no-deployment
-
 cd ${DIR}/../cloud_controller_ng
+bundle install --no-deployment
 
 language=${language:-csharp}
 
-# We can't run the db:create rake task, since it doesn't run mysql properly
-`mysql -e "create database cc_test;" -u root --password=password`
-
 # Remove existing api dir
 rm -rf ${DIR}/../cloud_controller_ng/doc/api/
-
 mv -f ${DIR}/../cloud_controller_ng/spec/api/documentation/templates/rspec_api_documentation/html_example.mustache ${DIR}/html_example.mustache.bk
 cp ${DIR}/html_example.mustache ${DIR}/../cloud_controller_ng/spec/api/documentation/templates/rspec_api_documentation/
 
